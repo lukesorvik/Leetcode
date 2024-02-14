8996c737d565b2d276913a05191cfc542252dd54
Luke Sorvik <lukesorvik@yahoo.com>
2024-02-13 23:12:33 -0800

    groupAnagrams done

--- groupAnagrams.java
+++ groupAnagrams.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -10,65 +11,34 @@ class groupAnagrams {
     }
 
     public static List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, Integer> mapStrings = new HashMap<>(); //create hashmap of (string, integer), string is the key, int is the group number
+        List<List<String>> ans = new ArrayList<>(); //create a list of lists of strings, each element is a list of strings
 
-        //given an array of strings group the anagrams together
-        
-        List<List<String>> result = new ArrayList<>(); //an arraylist of arraylists of strings, each element is an array of strings 
+        //for each string in the array of strings
+        //sort the string, then add it to the hashmap with the index of the list of strings that are anagrams of each other
+        //if the hashmap already contains the sorted string, then add the string to the list of strings at the index of the group number(same anagram group)
+        for (String str : strs) {
+            char[] chars = str.toCharArray(); //convert the string to a character array
+            Arrays.sort(chars); //sort the character array using java's sort method
+            String sortedStr = new String(chars); //convert the sorted character array back to a string
+            
 
-        //for each string check if it is an anagram of another string
-
-        for (int i = 0; i < strs.length; i++) {
-            List<String> anagramsOfStringI = new ArrayList<>(); //create a new arraylist of strings to store the anagrams
-            anagramsOfStringI.add(strs[i]); //add the current string to the arraylist
-
-            //check every other string in the list of strings to see if it is an anagram of the current string
-            for (int j = i + 1; j < strs.length; j++) {
-                //if the strings are anagrams, add them to the same arraylist
-                if (isAnagram(strs[i], strs[j])) { //check if the two strings are anagrams
-                    
-                    anagramsOfStringI.add(strs[j]); //add the anagram to the arraylist
-                    
-                }
+            if (mapStrings.containsKey(sortedStr)) { //if the hashmap contains the sorted string
+                ans.get(mapStrings.get(sortedStr)).add(str); 
+                //add the string to the list of strings at the index of the group number
+                //mapstrings.get geths the group number of the sorted string (group number is the index of the list of strings)
+            } else {
+                //if the hashmap does not contain the sorted string
+                mapStrings.put(sortedStr, ans.size()); //put the sorted string into the hashmap with the group number
+                //if the answer has nothing in it, then its group number is 0, if it has 1 element, then its group number is 1, etc 
+                //each group number is the index of the list of strings
+                ans.add(new ArrayList<>(Arrays.asList(str)));
             }
-            result.add(anagramsOfStringI); //add the arraylist of anagrams to the result arraylist
         }
 
-        return result;
+        return ans;
         
     }
 
 
-    //using a hash tablem O(n) time complexity, since we iterate through the strings once, then iterate through the hash table once (2n = O(n))
-    public static boolean isAnagram(String s, String t) {
-      Map<Character, Integer> count = new HashMap<>(); //create a hashmap, (character, integer)
-
-         // Count the frequency of characters in string s
-         for (char x : s.toCharArray()) {
-            if (count.containsKey(x)) {
-                count.put(x, count.get(x) + 1); //if the character is already in the map, increment the value by 1
-            } else {
-                count.put(x, 1); //if the character is not in the map, add it to the map with a value of 1
-            }
-        }
-        // Decrement the frequency of characters in string t
-        for (char x : t.toCharArray()) {
-            if (count.containsKey(x)) {
-                count.put(x, count.get(x) - 1); //if the character is already in the map, decrement the value by 1
-            } else {
-                return false; //if the character is not in the map, return false, not anagram
-            }
-        }
-
-        // Check if any character has non-zero frequency, if so then there was not an equal number of characters in both strings
-        for (int val : count.values()) {
-            //if the value is not 0, then the strings are not anagrams, unequal number of characters
-            if (val != 0) {
-                return false;
-            }
-        }
-        
-        //reached here, then the strings are anagrams
-        return true;
-
-    }
 }
