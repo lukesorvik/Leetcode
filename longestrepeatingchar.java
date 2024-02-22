f8114f08266df6d282f57f8239b7da018a54ff74
Luke Sorvik <lukesorvik@yahoo.com>
2024-02-21 20:52:16 -0800

    sliding window 2

--- longestrepeatingchar.java
+++ longestrepeatingchar.java
@@ -8,27 +8,15 @@
  * 
  * Solution:
  * use sliding window to go block by block
- * instead this time we can ignore current char being in set up to k times
+ * get the max frequency of any character within the block
+ * if the number of characters that are not the max frequency character is greater than k, move the left pointer to the right
+ * (since this block can only replace k characters to match the most frequent character, if there are more than k characters that are not the most frequent
+ * , we need to move the left pointer to the right since this block is invalid)
  * 
- * ex:
+ * we can store the frequency of every letter in a map, worst case we are given a string of 26 characters
+ * so O(26) = O(1) still constant time
  * 
- * ABAB
- * when we get to B, we see it is not in the set, so we check if we have used k yet, if not 
- * 
- * 
- * 
- * for length of string 
- * 
- * if rightpointer != char at left pointer && allowed excuses = 0
- *  then we iterate the left pointer until we reach the first char not equal to the original left pointer
- *  reset allowed freebies = k
- * if thing at right pointer is not duplicat
- *  allowed freebies -1
- * else 
- * the thing at the right pointer is a duplicate
- * update max length 
- * move right pointer over one
- * 
+ * we iterate over the string once using the rightpointer, we stop when we can no longer increment the right pointer so O(n) runtime
  * 
  */
 
@@ -37,21 +25,22 @@ import java.util.*;
 
 public class longestrepeatingchar {
     public static void main(String[] args) {
-        
+        System.out.println(characterReplacement("ABBAABB", 2) + " should be 6");
         System.out.println(characterReplacement("ABBB", 2) + " should be 4");
         System.out.println(characterReplacement("ABAB", 2) + " should be 4");
         System.out.println(characterReplacement("AABABBA", 1) + " should be 4");
         
+        
     }
 
     public static int characterReplacement(String s, int k) {
-        int longest = 0;
+        int longest = 0; //keep track of the longest substring
         Map<Character, Integer> map = new HashMap<>(); //map to store the frequency of a character
-        int leftpointer = 0;
-        int maxfreq = 0;
+        int leftpointer = 0; //left pointer to keep track of the start of the current block
+        int maxfreq = 0; //keep track of the max frequency of any character in the current block
 
         for (int rightpointer =0; rightpointer< s.length(); rightpointer++) {
-            Character c = s.charAt(rightpointer);
+            Character c = s.charAt(rightpointer); //get the character at the right pointer
 
             //if the character at the right pointer exists in the map
             if (map.containsKey(c)) {
@@ -66,26 +55,22 @@ public class longestrepeatingchar {
 
             //if the length of the current block - the max frequency of any character in the block is greater than k
             //length of block - max frequency of any character in the block = number of characters that are not the max frequency character
-            // # of chars that are not the most frequent should be <= k
+            // # of chars that are not the most frequent in current block should be <= k
             //if it is not, we need to move the left pointer to the right, and decrement the frequency of the character at the left pointer by 1
-            //move the current block to the right
+            //move the current block to the right, continue testing the current block
             if ((rightpointer - leftpointer + 1) - maxfreq > k) {
                 //move the left pointer to the right
                 map.put(s.charAt(leftpointer), map.get(s.charAt(leftpointer)) - 1); //decrement the frequency of the character at the left pointer by 1
                 leftpointer++; //move the left pointer to the right
             }
 
+            //the block is valid, does not break the k rule
             else {
                 //update the longest substring length
                 longest = Math.max(longest, rightpointer - leftpointer + 1);
             }
-
-
-
         }
-
         return longest;
-        
     }
 
     //iterate over the map, return the max frequency of any character
