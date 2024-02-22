d381ff3005a9a059305889ea1ae2f54afdd353f9
Luke Sorvik <lukesorvik@yahoo.com>
2024-02-21 19:15:55 -0800

    sliding window

--- longestsubstringnorepeat.java
+++ longestsubstringnorepeat.java
@@ -3,25 +3,37 @@ import java.util.*;
 public class longestsubstringnorepeat {
     public static void main(String[] args) {
         String s = "pwwkew";
+        //String s = "aab";
         System.out.println(lengthOfLongestSubstring(s));
     }
     
     public static int lengthOfLongestSubstring(String s) {
-        
         int longest = 0;
-        List<Character> substring = new ArrayList(); //arraylist to store current substring
+        Set<Character> set = new HashSet<>(); // to store the characters we have found so far (of our current block)
 
-        for (int right = 0; right< s.length(); right++ ) {
-            //if we have found a duplicate
-            if(substring.contains(s.charAt(right))) { 
-                int index = substring.indexOf(s.charAt(right));
-                substring.remove(index); //remove the element from the current substring 
-            }
-            else{
-                substring.add((Character)s.charAt(right));
-                longest = Math.max(longest, substring.size());
+        int leftpointer = 0;
 
+        for (int rightpointer = 0; rightpointer < s.length(); rightpointer++) {
+           char c = s.charAt(rightpointer);
+
+           //if the current character is already in the set, move the left pointer to the right until the current character is no longer in the set
+           //remove the left pointer character from the set as we move the left pointer to the right
+            if (set.contains(c)) {
+               while ( set.contains(c)) {
+                   set.remove(s.charAt(leftpointer));
+                   leftpointer++;
+               }
+               set.add(c); //add the current element to the set, to start a new block
+                
             }
+
+            //continue current block
+            //if the current character is not in the set, add it to the set and update the longest substring length
+            else {
+                set.add(c);
+                longest = Math.max(longest, rightpointer - leftpointer + 1);
+            }
+
         }
 
         return longest;
