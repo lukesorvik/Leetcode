f632d808108ef78019b81e8fa8636d6d81126582
Luke Sorvik <lukesorvik@yahoo.com>
2024-03-09 18:39:57 -0800

    working optimal sol

--- threesumOPT.java
+++ threesumOPT.java
@@ -19,34 +19,41 @@ public class threesumOPT {
      */
      public List<List<Integer>> threeSum(int[] nums) {
         Arrays.sort(nums);
-        LinkedList<List<Integer>> sol = new LinkedList<List<Integer>>();
+        LinkedList<List<Integer>> sol = new LinkedList<List<Integer>>(); //linked list to add to the front of the list
 
+        //iterate through the array, n-2 times since we are looking at 3 elements at a time
         for (int i = 0; i < nums.length - 2; i++) {
-            //Only consider non-duplicate elements for i
-            if (i == 0 || (i > 0 && nums[i] != nums[i - 1])) {
-                int target = 0 - nums[i];
-                int left = i + 1;
-                int right = nums.length - 1;
 
+            //Only consider non-duplicate elements for i
+            //if first element in array, or if the element is not a duplicate to the previous element
+            if (i == 0 || (i > 0 && nums[i] != nums[i - 1])) { //if the element is not a duplicate to the previous element
+                int target = 0 - nums[i]; //target is the complement of 0-num[i], or what we would need to add to nums[i] to get 0
+                int left = i + 1; //left pointer
+                int right = nums.length - 1; //right pointer, start at the end of the array
+
+                //while the left pointer is less than the right pointer, not equal
                 while (left < right) {
-                    if (nums[left] + nums[right] == target) {
-                        ArrayList<Integer> miniSol = new ArrayList<>();
+                    if (nums[left] + nums[right] == target) { //if the sum of the 3 elements is 0
+                        ArrayList<Integer> miniSol = new ArrayList<>(); //create a new arraylist to store the 3 elements
                         miniSol.add(nums[i]);
                         miniSol.add(nums[left]);
                         miniSol.add(nums[right]);
-                        sol.add(miniSol);
-                        while (left < right && nums[left] == nums[left + 1]) {
+                        sol.add(miniSol); //add the arraylist to solution
+                        //if the next element is a duplicate, skip it, and while the pointers are not the same
+                        while (left < right && nums[left] == nums[left + 1]) {  //iterate to the right until we find a non-duplicate element
                             left++;
                         }
-                        while (left < right && nums[right] == nums[right - 1]) {
+                        //if the next element is a duplicate, skip it, and while the pointers are not the same
+                        while (left < right && nums[right] == nums[right - 1]) { //iterate to the left until we find a non-duplicate element
                             right--;
                         }
-                        left++;
-                        right--;
-                    } else if (nums[left] + nums[right] > target) {
-                        right--;
-                    } else {
-                        left++;
+                        left++; //set the left pointer to the next element
+                        right--; //set the right pointer to the previous element
+
+                    } else if (nums[left] + nums[right] > target) { //if the sum of the 3 elements is greater than 0
+                        right--; //shift the right pointer to the left, so the sum is smaller
+                    } else { //if the sum of the 3 elements is less than 0
+                        left++; //shift the left pointer to the right, so the sum is larger
                     }
                 }
             }
